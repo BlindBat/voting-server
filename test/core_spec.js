028616ce4d8281521d16497ccb19bc82cb4f9710
alfa bank <alfabank@Mac-mini-alfa.local>
2017-05-29 09:10:47 +0600

    setEntries immutable logic

--- test/core_spec.js
+++ test/core_spec.js
@@ -5,9 +5,9 @@ import {setEntries} from '../src/core';
 
 describe('application logic', () => {
     describe('setEntries', () => {
-        it('добавляет записи к состоянию', () => {
+        it('преобразует в immutable', () => {
             const state = Map();
-            const entries = List.of('Trainspotting', '28 Days Later');
+            const entries = ['Trainspotting', '28 Days Later'];
             const nextState = setEntries(state, entries);
             expect(nextState).to.equal(Map({
                 entries: List.of('Trainspotting', '28 Days Later')
